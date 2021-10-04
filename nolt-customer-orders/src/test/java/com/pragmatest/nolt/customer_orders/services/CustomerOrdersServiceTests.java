8ecddd3d52b4d9a0b75d377b68d909479c4f8a87
clydevassallo <cooler364@gmail.com>
2021-10-05 01:20:12 +0200

    Add matcher in service tests

--- nolt-customer-orders/src/test/java/com/pragmatest/nolt/customer_orders/services/CustomerOrdersServiceTests.java
+++ nolt-customer-orders/src/test/java/com/pragmatest/nolt/customer_orders/services/CustomerOrdersServiceTests.java
@@ -1,6 +1,9 @@
 package com.pragmatest.nolt.customer_orders.services;
 
+import com.cedarsoftware.util.DeepEquals;
+import com.pragmatest.nolt.customer_orders.data.entities.CustomerOrderEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -27,5 +30,45 @@ class CustomerOrdersServiceTests {
         // Assert
         assertNotNull(id, "Id in response is null.");
         assertIsValidUuid(id);
+    }
+}
+
+class CustomerOrderEntityMatcher implements ArgumentMatcher<CustomerOrderEntity> {
+
+    private CustomerOrderEntity left;
+    private CustomerOrderEntity match;
+
+    public CustomerOrderEntityMatcher(CustomerOrderEntity left) {
+        this.left = left;
+    }
+
+    @Override
+    public boolean matches(CustomerOrderEntity right) {
+        boolean isMatch = left != null && right != null &&
+                left.getCustomerId().equals(right.getCustomerId()) &&
+                DeepEquals.deepEquals(left.getOrderItems(), right.getOrderItems()) &&
+                isValidUUID(right.getOrderId());
+
+        if (isMatch) {
+            match = right;
+        }
+
+        return isMatch;
+    }
+
+    public CustomerOrderEntity getMatch() {
+        return match;
+    }
+
+    private boolean isValidUUID(String id) {
+
+        if (id == null) return false;
+
+        try {
+            UUID uuid = UUID.fromString(id);
+            return id.equals(uuid.toString());
+        } catch(IllegalArgumentException e) {
+            return false;
+        }
     }
 }
