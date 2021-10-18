437710661f553e73596457e0a7789f56c05eb562
Andrea Mangion <andrea.mangion@gig.com>
2021-10-18 23:03:10 +0200

    Adding profile !test to be excluded during a test run.

--- nolt-restaurant-orders/src/main/java/com/pragmatest/nolt/restaurant_orders/configuration/DataPopulatorConfiguration.java
+++ nolt-restaurant-orders/src/main/java/com/pragmatest/nolt/restaurant_orders/configuration/DataPopulatorConfiguration.java
@@ -2,11 +2,13 @@ package com.pragmatest.nolt.restaurant_orders.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @Configuration
+@Profile("!test")
 public class DataPopulatorConfiguration {
 
     @Bean
