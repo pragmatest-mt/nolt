500350afa2f553d8db1d68a4dd3cb5efe165ccf6
Andrea Mangion <andrea.mangion@gig.com>
2021-10-18 23:03:23 +0200

    Removing unused autowiring.

--- nolt-restaurant-orders/src/main/java/com/pragmatest/nolt/restaurant_orders/RestaurantsOrderApplication.java
+++ nolt-restaurant-orders/src/main/java/com/pragmatest/nolt/restaurant_orders/RestaurantsOrderApplication.java
@@ -1,16 +1,11 @@
 package com.pragmatest.nolt.restaurant_orders;
 
-import com.pragmatest.nolt.restaurant_orders.data.repositories.RestaurantsOrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RestaurantsOrderApplication {
-
-    @Autowired
-    RestaurantsOrderRepository repository;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(RestaurantsOrderApplication.class, args);
